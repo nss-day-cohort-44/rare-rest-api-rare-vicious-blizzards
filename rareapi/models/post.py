5c0b1e1f60bbe5018fb4abd06a745c829c0f8f04
negative1skunk@gmail.com <negative1skunk@gmail.com>
2021-02-19 11:50:14 -0600

    removed required from Bools

--- rareapi/models/post.py
+++ rareapi/models/post.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     publication_date = models.DateField(auto_now=False, auto_now_add=False)
     post_image = models.ImageField(upload_to="profile_pics", max_length=None, width_field=None, height_field=None)
     content = models.CharField(max_length=5000)
-    approved = models.BooleanField(default=None, required=True)
+    approved = models.BooleanField(default=None)
     user = models.ForeignKey("RareUser", on_delete=models.CASCADE)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
  
--- rareapi/models/rare_user.py
+++ rareapi/models/rare_user.py
@@ -8,5 +8,5 @@ class RareUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_image = models.ImageField(upload_to="profile_pics", max_length=None, width_field=None, height_field=None)
     created_on = models.DateField(auto_now=False, auto_now_add=False)
-    active = models.BooleanField(default=None, required=True)
+    active = models.BooleanField(default=None)
     bio = models.CharField(max_length=50)
