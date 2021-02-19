4e62314bd32617b7b834d221cec75e1d25d5407e
negative1skunk@gmail.com <negative1skunk@gmail.com>
2021-02-19 13:11:37 -0600

    models migrated

--- rareapi/models/demotion_queue.py
+++ rareapi/models/demotion_queue.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class DemotionQueue(models.Model):
     action = models.CharField(max_length=200)
-    admin = models.ForeignKey("RareUser", on_delete=models.CASCADE)
-    approver_one = models.ForeignKey("RareUser", on_delete=models.CASCADE)
+    admin = models.ForeignKey("RareUser", related_name="admin", on_delete=models.CASCADE)
+    approver_one = models.ForeignKey("RareUser", related_name="approver", on_delete=models.CASCADE)
--- rareapi/models/subscription.py
+++ rareapi/models/subscription.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Subscription(models.Model):
-    author = models.ForeignKey("RareUser", on_delete=models.CASCADE)
-    follower = models.ForeignKey("RareUser", on_delete=models.CASCADE)
+    author = models.ForeignKey("RareUser", related_name="author", on_delete=models.CASCADE)
+    follower = models.ForeignKey("RareUser", related_name="follower", on_delete=models.CASCADE)
     created_on = models.DateField(auto_now=False, auto_now_add=False)
     ended_on = models.DateField(auto_now=False, auto_now_add=False)
