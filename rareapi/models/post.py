85d574fadc49947ab8a9de39524396561ffb1e4f
negative1skunk@gmail.com <negative1skunk@gmail.com>
2021-02-19 11:48:09 -0600

    models added but not migrated

---
+++ rareapi/models/post.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    publication_date = models.DateField(auto_now=False, auto_now_add=False)
+    post_image = models.ImageField(upload_to="profile_pics", max_length=None, width_field=None, height_field=None)
+    content = models.CharField(max_length=5000)
+    approved = models.BooleanField(default=None, required=True)
+    user = models.ForeignKey("RareUser", on_delete=models.CASCADE)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+ 
