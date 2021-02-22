78d4cd6ac3d45ea58f8317478aaa6d4ba31f1e01
Suganya <suganprakash0621@gmail.com>
2021-02-22 11:08:04 -0600

    fixed merge conflicts

--- rareapi/views/__init__.py
+++ rareapi/views/__init__.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 from .tag import Tags
-=======
-# from .auth import login_user
-# from .auth import register_user
 from .comment import CommentView
-# from .post import Post
->>>>>>> main
+
